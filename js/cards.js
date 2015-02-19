7bb761b54709eef0e42acb02bd1c8ad2ea3d1304
ross8 <rosson.creighton@gmail.com>
2015-02-19 17:33:31 -0500

    Setting up repo

---
+++ js/cards.js
@@ -0,0 +1,21 @@
+var devs = [
+  {
+    firstName: 'Emily',
+    lastName: 'Maskin',
+    role: 'Front End Lead',
+    email: 'emily@keep.com',
+    phone: '607-437-7716'
+  },
+  {
+    firstName: 'Gregg',
+    lastName: 'Meluski',
+    role: 'Senior Front End Engineer',
+    email: 'gregg@keep.com'
+  },
+  {
+    firstName: 'Ryan',
+    lastName: 'Quigley',
+    role: 'CTO',
+    email: 'ryan@keep.com'
+  }
+]
