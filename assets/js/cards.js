30deb4b7a8e2c74d0fea3c79aa19dfaa8b0c2a6b
RossCodes <rosson.creighton@gmail.com>
2015-02-27 00:42:40 -0500

    Finishing touches

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -18,33 +18,33 @@ var devs = [{
 }];
 
 //Card object constructor
-	function Card(firstName, lastName, role, email, phone, id) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.role = role;
-		this.email = email;
-		this.phone = phone;
-		this.id = id;
+function Card(firstName, lastName, role, email, phone, id) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.role = role;
+    this.email = email;
+    this.phone = phone;
+    this.id = id;
 
-		//Array with instructions for building each component of the cards
-		//[component type(element or text),component class, component id, component text, location to be inserted,phone number component(boolean)]
-		this.components = [
-			['el', 'col-xs-12 col-md-4', 'card-col' + this.id, , 'card-area'], //bootstrap column for each card
-			['el', 'card-container col-xs-12', 'card-container' + this.id, , 'card-col' + this.id], //outer container for each card
-			['el', 'left-container card-height col-xs-6', 'left-container' + this.id, , 'card-container' + this.id], //inner container for left half of card
-			['el', 'right-container card-height col-xs-6', 'right-container' + this.id, , 'card-container' + this.id], //inner container for right half of card
-			['el', 'logo-container center-vertical center-block', 'logo-container' + this.id, , 'left-container' + this.id], //inner container for logo
-			['el', 'info-container center-vertical', 'info-container' + this.id, , 'right-container' + this.id], //inner container for dev info
-			['el', 'name-line', 'name-line' + this.id, , 'info-container' + this.id], //div for full name
-			['txt', , , this.firstName + " " + this.lastName, 'name-line' + this.id], //full name text
-			['el', 'role-line', 'role-line' + this.id, , 'info-container' + this.id], //div for role
-			['txt', , , this.role, 'role-line' + this.id], //role text
-			['el', 'email-line', 'email-line' + this.id, , 'info-container' + this.id], //div for email address
-			['txt', , , this.email, 'email-line' + this.id], //email address text
-			['el', 'phone-line', 'phone-line' + this.id, , 'info-container' + this.id, true], //div for phone number
-			['txt', , , this.phone, 'phone-line' + this.id, true] //phone number text
-		];
-	};
+    //Array with instructions for building each component of the cards
+    //[component type(element or text),component class, component id, component text, location to be inserted,phone number component(boolean)]
+    this.components = [
+        ['el', 'col-xs-12 col-md-4', 'card-col' + this.id, , 'card-area'], //bootstrap column for each card
+        ['el', 'card-container col-xs-12', 'card-container' + this.id, , 'card-col' + this.id], //outer container for each card
+        ['el', 'left-container card-height col-xs-6', 'left-container' + this.id, , 'card-container' + this.id], //inner container for left half of card
+        ['el', 'right-container card-height col-xs-6', 'right-container' + this.id, , 'card-container' + this.id], //inner container for right half of card
+        ['el', 'logo-container center-vertical center-block', 'logo-container' + this.id, , 'left-container' + this.id], //inner container for logo
+        ['el', 'info-container center-vertical', 'info-container' + this.id, , 'right-container' + this.id], //inner container for dev info
+        ['el', 'name-line', 'name-line' + this.id, , 'info-container' + this.id], //div for full name
+        ['txt', , , this.firstName + " " + this.lastName, 'name-line' + this.id], //full name text
+        ['el', 'role-line', 'role-line' + this.id, , 'info-container' + this.id], //div for role
+        ['txt', , , this.role, 'role-line' + this.id], //role text
+        ['el', 'email-line', 'email-line' + this.id, , 'info-container' + this.id], //div for email address
+        ['txt', , , this.email, 'email-line' + this.id], //email address text
+        ['el', 'phone-line', 'phone-line' + this.id, , 'info-container' + this.id, true], //div for phone number
+        ['txt', , , this.phone, 'phone-line' + this.id, true] //phone number text
+    ];
+};
 
 //Create Method prints cards to card display area
 Card.prototype.create = function() {
@@ -55,21 +55,21 @@ Card.prototype.create = function() {
 	//Loop through components[] array to add components to card
 	for (var i = 0; i < this.components.length; i++) {
         
-        var components = this.components[i];
+        var component = this.components[i];
 
-		if (components[5] && !this.phone) { //check if phone number is given
+		if (component[5] && !this.phone) { //check if phone number is given
 			//do not create phone component if not defined
 
-		} else if (this.components[i][0] == 'el') { //create element and append to DOM
+		} else if (component[0] == 'el') { //create element and append to DOM
 			var element = document.createElement('div');
-			element.setAttribute('class', components[1]);
-			element.setAttribute('id', components[2]);
-			var location = document.getElementById(components[4]);
+			element.setAttribute('class', component[1]);
+			element.setAttribute('id', component[2]);
+			var location = document.getElementById(component[4]);
 			location.appendChild(element);
 
-		} else if (this.components[i][0] == 'txt') { //create text node and append to DOM
-			var element = document.createTextNode(components[3]);
-			var location = document.getElementById(components[4]);
+		} else if (component[0] == 'txt') { //create text node and append to DOM
+			var element = document.createTextNode(component[3]);
+			var location = document.getElementById(component[4]);
 			location.appendChild(element);
             
 		};
